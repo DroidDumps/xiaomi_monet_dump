fecbbb4c5b1ebdf5956f99c308ec9a0d04212d9f
DroidDumps <rokibhasansagar2014@outlook.com>
2020-12-14 15:47:23 +0000

    Add extras for qssi-user 11 RKQ1.200826.002 V12.1.1.0.RJIMIXM release-keys
    Signed-off-by: DroidDumps <rokibhasansagar2014@outlook.com>

---
+++ join_split_files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cat cust/app/customized/partner-video.like_111/partner-video.like_111.apk.* 2>/dev/null >> cust/app/customized/partner-video.like_111/partner-video.like_111.apk
+rm -f cust/app/customized/partner-video.like_111/partner-video.like_111.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk.* 2>/dev/null >> cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk
+rm -f cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk.* 2>/dev/null
+cat cust/app/customized/partner-ctrip.english_34/partner-ctrip.english_34.apk.* 2>/dev/null >> cust/app/customized/partner-ctrip.english_34/partner-ctrip.english_34.apk
+rm -f cust/app/customized/partner-ctrip.english_34/partner-ctrip.english_34.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk.* 2>/dev/null >> cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk
+rm -f cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk.* 2>/dev/null
+cat vendor/camera/mimoji/model2.zip.* 2>/dev/null >> vendor/camera/mimoji/model2.zip
+rm -f vendor/camera/mimoji/model2.zip.* 2>/dev/null
+cat bootimg/05_dtbdump_S*1cH.dtb.* 2>/dev/null >> bootimg/05_dtbdump_S*1cH.dtb
+rm -f bootimg/05_dtbdump_S*1cH.dtb.* 2>/dev/null
+cat bootRE/boot.elf.* 2>/dev/null >> bootRE/boot.elf
+rm -f bootRE/boot.elf.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/app/MiuiWallpaper/MiuiWallpaper.apk.* 2>/dev/null >> system/system/app/MiuiWallpaper/MiuiWallpaper.apk
+rm -f system/system/app/MiuiWallpaper/MiuiWallpaper.apk.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk
+rm -f system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
